5df537c7d2931bdced5483d1ff6906f2e1518c62
CarnageDinkiDi <44189151+CarnageDinkiDi@users.noreply.github.com>
2019-06-11 12:06:32 +0530

    Commit

---
+++ Assets/CharacterChange.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class CharacterChange : MonoBehaviour
+{
+   public bool Characterone;
+    public bool CharacterTwo;
+
+    public PlayerController one;
+    public PlayerController two;
+
+    public Camera FirstCamera;
+    public Camera SecondCamera;
+
+    public Camera TransitionCamera;
+
+    private void Awake()
+    {
+        FirstCamera.gameObject.SetActive(true);
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Joystick Change"))
+        {
+            if (Characterone == true)
+            {
+                one.canMove = false;
+                two.canMove = true;
+                // SecondCamera.tag="MainCamera";
+                // FirstCamera.tag = "Untagged";
+                TransitionCamera.transform.position = SecondCamera.transform.position;
+                TransitionCamera.transform.eulerAngles = SecondCamera.transform.eulerAngles;
+
+                FirstCamera.gameObject.SetActive(false);
+                TransitionCamera.transform.DOMove(SecondCamera.transform.position, 0.5f);
+                StartCoroutine(waits(FirstCamera.gameObject,SecondCamera.gameObject,false));
+
+                //SecondCamera.gameObject.SetActive(true);
+               // FirstCamera.gameObject.SetActive(false);
+                //Characterone = false;
+            }
+            else if (Characterone == false)
+            {
+                one.canMove = true;
+                two.canMove = false;
+                //FirstCamera.tag = "MainCamera";
+                // SecondCamera.tag ="Untagged";
+                TransitionCamera.transform.position = FirstCamera.transform.position;
+                TransitionCamera.transform.eulerAngles = FirstCamera.transform.eulerAngles;
+
+                SecondCamera.gameObject.SetActive(false);
+                TransitionCamera.transform.DOMove(FirstCamera.transform.position, 0.5f);
+                StartCoroutine(waits(SecondCamera.gameObject, FirstCamera.gameObject, true));
+
+                //FirstCamera.gameObject.SetActive(true);
+               // SecondCamera.gameObject.SetActive(false);
+
+               // Characterone = true;
+            }
+        }
+    }
+
+    IEnumerator waits(GameObject OneCamera,GameObject TwoCamera,bool WhichCharacter)
+    {
+        yield return new WaitForSeconds(2f);
+      //  OneCamera.SetActive(false);
+        TwoCamera.SetActive(true);
+
+        Characterone = WhichCharacter;
+    }
+}
---
+++ Assets/Menu.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Menu : MonoBehaviour
+{
+    public List<Points> PointsList = new List<Points>();
+    public GameObject pivotPrefab;
+    public Transform planet;
+    public float timeToGo;
+    [Range (0,2)]
+    public int MenuSelect=0;
+
+    private void Start()
+    {
+        planet = transform.GetChild(0);
+
+        for(int i = 0; i < PointsList.Count; i++)
+        {
+            GameObject pivot = Instantiate(pivotPrefab, planet);
+            pivot.transform.eulerAngles = new Vector3(PointsList[i].points.y,-PointsList[i].points.x,0f);
+        }
+        CameraLook(0);
+    }
+    private void Update()
+    {
+
+        if (Input.GetButtonDown("R1"))
+        {
+            MenuSelect++;
+            if (MenuSelect <3)
+            {
+               
+                CameraLook(MenuSelect);
+            }
+            else
+            {
+                MenuSelect = 0;
+                CameraLook(MenuSelect);
+            }
+
+        }
+        else if (Input.GetButtonDown("L1"))
+        {
+            MenuSelect--;
+            if (MenuSelect>-1)
+            {
+                CameraLook(MenuSelect);
+            }
+            else
+            {
+                MenuSelect = 2;
+                CameraLook(MenuSelect);
+            }
+        }
+
+
+    }
+
+    void CameraLook(int index)
+    {
+        Points _points = PointsList[index];
+        Vector3 rot = new Vector3(_points.points.y,- _points.points.x,0f);
+        Camera.main.transform.parent.DORotate(rot, timeToGo, RotateMode.Fast);
+    }
+}
+
+[System.Serializable]
+public class Points
+{
+    public string name;
+    public Vector2 points;
+}
